640dc4db32c50a7ea662350135dec107dcd0bb28
Alessandro <sandroledesma@gmail.com>
2024-04-26 10:56:58 -0400

    update events branch

--- phase-1/events/introduction/beginning/index.js
+++ phase-1/events/introduction/beginning/index.js
@@ -25,5 +25,25 @@ Takeaways
 // 2. [medium] Add some non-click, non-submit event listener to the page.
 // 3. [hard] Add an image, and a form with one text input to the page.
 //    On submit, have the image change to the URL specified in the text input.
+<<<<<<< Updated upstream
+=======
+
+document.querySelector("#forms").addEventListener("submit", (event) => {
+    event.preventDefault(); 
+
+const imageUrl = document.querySelector("#image-url").value;
+const currentImg = document.querySelector("#my-image");
+
+if(currentImg) {
+    currentImg.src = imageUrl;
+} else {
+    const anotherImage = document.createElement('img');
+    anotherImage.src = "https://t3.ftcdn.net/jpg/03/14/10/26/360_F_314102633_ZDcyjOOkPwqPuucgbxF5h1e7GonSLR1k.jpg";
+    anotherImage.id = "my-new-image";
+    anotherImage.style.height = '200px';
+    document.body.append(anotherImage);
+})};
+
+>>>>>>> Stashed changes
 // 4. [bonus] Add an event handler to #today so that whenever some or all of its text is copied,
 //    that text is set as the content of #sometime.

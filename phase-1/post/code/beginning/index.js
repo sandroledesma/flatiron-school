c48970c3b3c75df42d1245d58882cf71e7873ef2
Alessandro <sandroledesma@gmail.com>
2024-04-29 16:14:23 -0400

    add files

--- phase-1/post/code/beginning/index.js
+++ phase-1/post/code/beginning/index.js
@@ -10,14 +10,54 @@ Objectives
 1. Make a GET request to display a dataset.
 2. Make a POST request to add to that dataset.
 
+POST basically means we are creating our own DATA SET. 
+
 */
 
 // Let's try making a GET request to display existing data on the page.
 
+fetch("http://localhost:3000/dogs")
+.then((response) => response.json()) // if using curly braces, return must be added {return response.json()}
+.then(dogs => {
+    const ul = document.querySelector("#dogs");
+    for (let i = 0; i < dogs.length; i++) {  // i++ is basically "i = i + 1
+        console.log(dogs[i]);
+    }; // for loop code does not include any append function (FYI)
+    dogs.forEach(dog => { // this is the same than the for loop above
+        const li = document.createElement("li");
+        li.textContent = (dog["name"] + " (" + dog["age"] + ")");
+        ul.append(li);
+    }); 
+});
+
 // Now, let's trigger a POST request when the user submits the form,
 // so that they can add data to the database! Remember to think about
 // the event, the target, and the handler when planning a listener.
-
+document.querySelector("form").addEventListener("submit", event => {
+    event.preventDefault();
+    const dogName = event.target["name"].value;
+    const dogAge = event.target["age"].value;
+    console.log(dogName + dogAge);
+    fetch("http://localhost:3000/dogs/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: dogName,
+            age: dogAge, 
+            isGoodDog: true
+        })
+    })
+    .then(response => response.json())
+    .then(newDog => { 
+        const ul = document.querySelector("#dogs");
+        const li = document.createElement("li");
+        li.textContent = (newDog["name"] + " (" + newDog["age"] + ")");
+        ul.append(li);
+        console.log(newDog);}) // for POST requests, the 2nd .then function input should be the new object
+});
 
 // ~ Challenges
 // 1. There are a handful of awfully similar lines in our requests. Try abstracting them

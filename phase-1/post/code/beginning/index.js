552638a060820a60db3632d632e3f37cc76d2454
Alessandro <sandroledesma@gmail.com>
2024-04-29 21:27:33 -0400

    update files

--- phase-1/post/code/beginning/index.js
+++ phase-1/post/code/beginning/index.js
@@ -14,27 +14,86 @@ POST basically means we are creating our own DATA SET.
 
 */
 
+function createAndAppendListItem(parentElement, text) { 
+    // this function creates and appends a new list item (li) via a parent element (ul) and the corresponding text output (li.textContent = "")
+    // const ul = document.querySelector("dogs"); is not in this consolidated function because it needs to select the right query (dogs or cats)
+    const li = document.createElement("li"); // originally in forEach statement
+    li.textContent = text; // originally in forEach statement as li.textContent = (dog["name"] + " (" + dog["age"] + ")");
+    parentElement.append(li); // originally in forEach statement as ul.append(li) -> which is why the function includes parentElement
+}
+
 // Let's try making a GET request to display existing data on the page.
 
 fetch("http://localhost:3000/dogs")
 .then((response) => response.json()) // if using curly braces, return must be added {return response.json()}
-.then((dogs) => {    
+.then((dogs) => { 
+    console.log("Dogs:", dogs);
     const ul = document.querySelector("#dogs");
-    dogs.forEach(dog => { // this is the same than the for loop above
-        const li = document.createElement("li");
-        li.textContent = (dog["name"] + " (" + dog["age"] + ")");
-        ul.append(li);
+    dogs.forEach(dog => { 
+        createAndAppendListItem(ul, `${dog.name} (${dog.age})`);
     }); 
 });
 
-// Now, let's trigger a POST request when the user submits the form,
-// so that they can add data to the database! Remember to think about
-// the event, the target, and the handler when planning a listener.
-document.querySelector("form").addEventListener("submit", event => {
+fetch("http://localhost:3000/cats") // repeated fetch code for the cats db
+.then((response) => response.json()) 
+.then((cats) => {    
+    const ul = document.querySelector("#cats");
+    cats.forEach(cat => { 
+        createAndAppendListItem(ul, `${cat.name} (${cat.age})`);
+    }); 
+});
+
+// Now, let's trigger a POST request when the user submits the form, so that they can add data to the database! 
+// Remember to think about the event, the target, and the handler when planning a listener.
+
+/* CODE FOR DROP DOWN "ADD A PET" feature (getting .value issues)
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    const form = document.querySelector("form"); // create a const for the typical 'document.QuerySelector("form") part of the event listener
+
+    form.addEventListener("submit", event => {
     event.preventDefault();
-    const dogName = event.target["name"].value;
-    const dogAge = event.target["age"].value;
-    console.log(dogName + dogAge);
+    
+    const petName = event.target["petName"].value;
+    const petAge = event.target["petAge"].value;
+    const petType = event.target.priority.value; 
+
+    // create if...else statement for the hosts 
+    let endpoint;
+    if (petType === "newDog") {
+        endpoint = "http://localhost:3000/dogs/";
+    } else if (petType === "newCat") {
+        endpoint = "http://localhost:3000/cats/";
+    }
+
+    fetch(endpoint, { // instead of fetch("http://localhost:3000/dogs/", since we have created an endpoint if...else statement
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: petName,
+            age: petAge 
+            // isGoodDog: true // is it possible to add other parameters that are not shared between dogs and cats db? 
+        })
+    })
+    .then(response => response.json())
+    .then(newPet => {
+        const ul = document.querySelector(petType === "newDog" ? "#dogs" : "#cats") //instead of ("#dogs"), enter a ternary statement 
+                                       // if petType is equal to newDog, return #dogs. else, return #cats 
+        createAndAppendListItem(ul, `${newPet.name} (${newPet.age})`);                    
+    })
+});
+}); */
+
+document.addEventListener("DOMContentLoaded", (event) => {
+
+document.querySelector("form[name='dogForm']").addEventListener("submit", event => {
+    event.preventDefault();
+    const dogName = event.target["dogName"].value;
+    const dogAge = event.target["dogAge"].value;
+    
     fetch("http://localhost:3000/dogs/", {
         method: "POST",
         headers: {
@@ -44,21 +103,45 @@ document.querySelector("form").addEventListener("submit", event => {
         body: JSON.stringify({
             name: dogName,
             age: dogAge, 
-            isGoodDog: true
+            isAGoodDog: true
         })
     })
     .then(response => response.json())
     .then(newDog => { 
         const ul = document.querySelector("#dogs");
-        const li = document.createElement("li");
-        li.textContent = (newDog["name"] + " (" + newDog["age"] + ")");
-        ul.append(li);
-        console.log(newDog);}) // for POST requests, the 2nd .then function input should be the new object
+        createAndAppendListItem(ul, `${newDog.name} (${newDog.age})`);
+        console.log(newDog);})
+});
+
+document.querySelector("form[name='catForm']").addEventListener("submit", event => {
+    event.preventDefault();
+    const catName = event.target["catName"].value;
+    const catAge = event.target["catAge"].value;
+    
+    fetch("http://localhost:3000/cats/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: catName,
+            age: catAge, 
+            isACat: true
+        })
+    })
+    .then(response => response.json())
+    .then(newCat => { 
+        const ul = document.querySelector("#cats");
+        createAndAppendListItem(ul, `${newCat.name} (${newCat.age})`);
+        console.log(newCat);})
+});
+
 });
 
 // ~ Challenges
 // 1. There are a handful of awfully similar lines in our requests. Try abstracting them
-//    into a function!
-// 2. Try writing your own POST request (for Cats).
+//    into a function! DONE
+// 2. Try writing your own POST request (for Cats). DONE
 // 2.5. Replace the forms with an Add a Pet form where you can add a dog or a cat
-// 3. Try writing PATCH and DELETE requests!
+//  3. Try writing PATCH and DELETE requests
